9dcd391b071a3232b9537a94bb0c50817612eff9
Pranab Kumar Sahoo <156513410+PranabKumar27@users.noreply.github.com>
2024-08-21 12:57:50 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,27 @@
+/* Types of Variable used in JavaScript */
+
+const accountId= 0001 
+let accountEmail= "pranabkumarsahoo27@gmail.com"
+var password="2983993"
+accountCity="bengaluru"
+
+
+//const is unchangable.
+//  accountId=20335;
+
+
+/* Value Changed Again */
+accountEmail="pranabkumar27@gmail.com"
+password='484994'
+accountCity="Odisha"
+
+
+/*
+Prefer not to use var 
+because of issue in block scope and functional Scope.
+ */
+
+/* For printing Purpose use 👇🏻*/
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountCity,password])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict" ;//treate all JS code as newer version.
+
+
+// alert("Hello") // We are using nodejs, not browser
+
+console.log(3
+    +3) //code readability should be high 
+    
+
+    /* Primitive DataTypes */
+
+//number=>2 to power 53
+//bigint=> if number is not sufficient then it should use .
+//String =>""
+//boolean => true/false
+//null=> standalone value
+// undefined ==> which is declared but not defined.
+// symbol => unique
+
+
+
+      /* Non-Primitive DataTypes */
+// object
+
+
+let age=24;
+
+console.log(typeof age)
+console.log(typeof "hitesh")
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
